6ea0270f9f14aecd028900c8a8185c1fd9a31842
Peter Bodechtel <sweetlikepete@gmail.com>
2019-01-04 10:55:19 +0200

    made some more tweaks to try and get around dpc_watchdog_violation errors that were happening when running the script in the linux subsystem under windows

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const fetch = require("node-fetch");
 const path = require("path");
 const fs = require("fs-extra");
 const args = require("yargs").argv;
+const download = require("image-downloader");
+
 
 const dest = path.resolve(args.dest);
 const requestBatchSize = 100;
-const artistIdBatchSize = 10;
+const artistIdBatchSize = 20;
 const idFolder = dest;
 const idFile = path.join(idFolder, "id.json");
+const writes = {};
 
 let id = 10000;
 
+// Load the id stored when the process was terminated through control+c
 if(fs.existsSync(idFile)){
 
     const rawId = fs.readFileSync(idFile);
@@ -21,6 +25,7 @@ if(fs.existsSync(idFile)){
 
 }
 
+// Force that a destination be specified
 if(!args.dest){
 
     console.log("Please specify a folder to save images to with the --dest option");
@@ -106,63 +111,53 @@ const get = async function(){
             await Promise.all(albums.map(async (album) => {
 
                 const cid = album.collectionId;
-                const albumFolder = path.join(dest, `data/album/${ String(cid).substring(0, 3) }`);
-                const albumFile = path.join(albumFolder, `${ cid }.json`);
-                const albumFileExists = await fs.exists(albumFile);
 
-                if(!albumFileExists){
+                if(!writes[String(cid)]){
+    
+                    writes[String(cid)] = true;
 
-                    await ensureDir(albumFolder);
-                    await fs.writeFile(albumFile, JSON.stringify(album, null, 4));
+                    const albumFolder = path.join(dest, `data/album/${ String(cid).substring(0, 3) }`);
+                    const albumFile = path.join(albumFolder, `${ cid }.json`);
+                    const albumFileExists = await fs.exists(albumFile);
+                    const artworkUrl = album.artworkUrl100.replace("100x100bb.jpg", "500x500bb.jpg");
+                    const imageFolder = path.join(dest, `images/album/${ String(cid).substring(0, 3) }`);
+                    const imageFile = path.join(imageFolder, `${ cid }.jpg`);
+                    const imageFileExists = await fs.exists(imageFile);
 
-                }
+                    if(!albumFileExists){
 
-                const artworkUrl = album.artworkUrl100.replace("100x100bb.jpg", "500x500bb.jpg");
-
-                const imageFolder = path.join(dest, `images/album/${ String(cid).substring(0, 3) }`);
-                const imageFile = path.join(imageFolder, `${ cid }.jpg`);
-                const imageFileExists = await fs.exists(imageFile);
-
-                if(!imageFileExists){
-
-                    await ensureDir(imageFolder);
-
-                    const res = await fetch(artworkUrl);
-
-                    await new Promise((resolve, reject) => {
-
-                        const fileStream = fs.createWriteStream(imageFile, {
-                            autoClose: true
-                        });
-
-                        res.body.pipe(fileStream);
-
-                        res.body.on("error", (err) => {
-
-                            console.error(`${ idTag } - Download failed - ${ cid }`);
-
-                            fileStream.close();
-
-                            reject(err);
-
-                        });
-
-                        fileStream.on("finish", function() {
-
+                        await ensureDir(albumFolder);
+                        await fs.writeFile(albumFile, JSON.stringify(album, null, 4));
+    
+                    }
+    
+                    if(!imageFileExists){
+    
+                        try {
+                            
+                            await download.image({
+                                url: artworkUrl,
+                                dest: imageFile
+                            });
+                            
                             console.log(`${ idTag } - Downloaded artwork - ${ cid }`);
 
-                            fileStream.close();
+                        }catch(e){
+                        
+                            console.error(`${ idTag } - Download failed - ${ cid }`);
 
-                            resolve();
+                            console.error(e);
+                        
+                        }
 
-                        });
+                        writes[String(cid)] = false;
 
-                    });
-
-                }else{
-
-                    console.log(`${ idTag } - Already Downloaded artwork - ${ cid }`);
-
+                    }else{
+    
+                        console.log(`${ idTag } - Already Downloaded artwork - ${ cid }`);
+    
+                    }
+    
                 }
 
             }));
@@ -178,6 +173,11 @@ const get = async function(){
     await Promise.all(promises);
 
     id += (requestBatchSize * artistIdBatchSize);
+   
+    await ensureDir(idFolder);
+    await fs.writeFile(idFile, JSON.stringify({
+        id: id
+    }, null, 4));
 
     get();
 
@@ -185,13 +185,4 @@ const get = async function(){
 
 get();
 
-process.on("SIGINT", async () => {
-      
-    await ensureDir(idFolder);
-    await fs.writeFile(idFile, JSON.stringify({
-        id: id - (requestBatchSize * artistIdBatchSize)
-    }, null, 4));
 
-    process.exit();
-
-});
